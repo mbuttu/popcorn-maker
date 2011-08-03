1c98483064845abc191293fb77c7b711c5ddeb51
Mohammed Buttu <mohammedbuttu@gmail.com>
2011-08-03 13:56:45 -0400

    Put target div for timeline in an iframe

--- js/popcorn-maker.js
+++ js/popcorn-maker.js
@@ -1,42 +1,60 @@
-(function(){
+(function() {
 
-  window.addEventListener("DOMContentLoaded", function(){
-    
-    var b  = new Butter();
+	window.addEventListener("DOMContentLoaded", function() {
 
-    b.eventeditor( { target: "butter-editor-div", defaultEditor: "lib/defaultEditor.html", editorWidth: "98%", editorHeight: "98%"  } );
+		var b = new Butter();
 
-    b.previewer({
-      layout: "layouts/default.html",
-      target: "previewer-iframe",
-      popcornURL: "lib/popcorn-complete.js",
-      media: "http://robothaus.org/bugs/video/brendan1.ogv",
-      callback: function() {
-        b.buildPopcorn( "outerVideo", function() {
-          b.plugintray({ target: "butter-plug-in-div" });
-          var registry = b.getRegistry();
-          for( var i = 0, l = registry.length; i < l; i ++ ) {
-                b.addPlugin( { type: registry[ i ].type } );
-          }
-        } );
-      }
-    });
-    
-    b.timeline({ target: "butter-timeline-div"});
-  
-    b.listen ( "trackeditstarted", function() {
-      overlay( "visible" );
-    });
-    b.listen ( "trackeditclosed", function() {
-      overlay( "hidden" );
-    });
-    
-  }, false);
+		b.eventeditor({
+			target: "butter-editor-div",
+			defaultEditor: "lib/defaultEditor.html",
+			editorWidth: "98%",
+			editorHeight: "98%"
+		});
 
-  function overlay( style ) {
-    el = document.getElementById( "overlay-div" );
-    el.style.visibility = style;
-  }
+		b.previewer({
+			layout: "layouts/default.html",
+			target: "previewer-iframe",
+			popcornURL: "lib/popcorn-complete.js",
+			media: "http://robothaus.org/bugs/video/brendan1.ogv",
+			callback: function() {
+				b.buildPopcorn("outerVideo", function() {
+					b.plugintray({
+						target: "butter-plug-in-div"
+					});
+					var registry = b.getRegistry();
+					for (var i = 0, l = registry.length; i < l; i++) {
+						b.addPlugin({
+							type: registry[i].type
+						});
+					}
+				});
+			}
+		});
+
+		var iframe = document.getElementById("iframe2");
+		iframe.addEventListener("load", function(e) {
+			var myDiv = iframe.contentWindow.document.getElementById("mydiv");
+      b.timeline({
+        //target: "butter-timeline-div"
+        target: myDiv
+      });
+		}, false);
+
+
+		b.listen("trackeditstarted", function() {
+			overlay("visible");
+		});
+		b.listen("trackeditclosed", function() {
+			overlay("hidden");
+		});
+
+	},
+	false);
+
+	function overlay(style) {
+		var el = document.getElementById("overlay-div");
+		el.style.visibility = style;
+	}
 
 })();
 
