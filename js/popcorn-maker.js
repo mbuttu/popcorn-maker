a25b75b60e51c8ff1e36d953dee055aca143117b
Mohammed Buttu <mohammedbuttu@gmail.com>
2011-08-08 19:46:14 -0400

    Use movie provided by user and track events specified (as markers) in Final Cut Pro

--- js/popcorn-maker.js
+++ js/popcorn-maker.js
@@ -1,42 +1,87 @@
-(function(){
+(function() {
 
-  window.addEventListener("DOMContentLoaded", function(){
-    
-    var b  = new Butter();
+	window.addEventListener("DOMContentLoaded", function() {
 
-    b.eventeditor( { target: "butter-editor-div", defaultEditor: "lib/defaultEditor.html", editorWidth: "98%", editorHeight: "98%"  } );
+		var b = new Butter(),
+		appController = window.appController,
+		tracks = JSON.parse(appController.tracksAsJSON()),
+		idx;
 
-    b.previewer({
-      layout: "layouts/default.html",
-      target: "previewer-iframe",
-      popcornURL: "lib/popcorn-complete.js",
-      media: "http://robothaus.org/bugs/video/brendan1.ogv",
-      callback: function() {
-        b.buildPopcorn( "outerVideo", function() {
-          b.plugintray({ target: "butter-plug-in-div" });
-          var registry = b.getRegistry();
-          for( var i = 0, l = registry.length; i < l; i ++ ) {
-                b.addPlugin( { type: registry[ i ].type } );
-          }
-        } );
-      }
-    });
-    
-    b.timeline({ target: "butter-timeline-div"});
-  
-    b.listen ( "trackeditstarted", function() {
-      overlay( "visible" );
-    });
-    b.listen ( "trackeditclosed", function() {
-      overlay( "hidden" );
-    });
-    
-  }, false);
+    console.log(appController.tracksAsJSON());
 
-  function overlay( style ) {
-    el = document.getElementById( "overlay-div" );
-    el.style.visibility = style;
-  }
+		var targetDiv = appController.editorDOMWindow.document.createElement("div");
+
+		targetDiv.id = "me";
+		targetDiv.style.width = "400px";
+		targetDiv.style.height = "500px";
+		targetDiv.style.border = "1px solid black";
+		appController.editorDOMWindow.document.body.appendChild(targetDiv);
+
+		b.eventeditor({
+			//target: "butter-editor-div",
+			//defaultEditor: "lib/defaultEditor.html",
+			target: targetDiv,
+			defaultEditor: "popcorn-maker/lib/defaultEditor.html",
+			editorWidth: "98%",
+			editorHeight: "98%"
+		});
+
+		b.previewer({
+			layout: "layouts/default.html",
+			target: "previewer-iframe",
+			popcornURL: "lib/popcorn-complete.js",
+			//media: "http://robothaus.org/bugs/video/brendan1.ogv",
+			//media: "trailer.mp4",
+      media: appController.moviePath,
+			callback: function() {
+				b.buildPopcorn("outerVideo", function() {
+					for (idx = 0; idx < tracks.length; idx++) {
+						var events = tracks[idx].events;
+						var eventIdx;
+
+						var butterTrack = new Butter.Track();
+						b.addTrack(butterTrack);
+
+						for (eventIdx = 0; eventIdx < events.length; eventIdx++) {
+							var event = events[eventIdx];
+console.log(event);
+
+							var butterTrackEvent = new Butter.TrackEvent({
+								type: event.type,
+								popcornOptions: {
+									start: event.start,
+									end: event.end
+								}
+							});
+
+							butterTrackEvent.track = butterTrack;
+							b.addTrackEvent(butterTrack, butterTrackEvent);
+						}
+					}
+					b.plugintray({
+						target: "butter-plug-in-div"
+					});
+					var registry = b.getRegistry();
+					for (var i = 0, l = registry.length; i < l; i++) {
+						b.addPlugin({
+							type: registry[i].type
+						});
+					}
+				});
+			}
+		});
+
+		b.timeline({
+			target: "butter-timeline-div"
+		});
+
+	},
+	false);
+
+	function overlay(style) {
+		var el = document.getElementById("overlay-div");
+		el.style.visibility = style;
+	}
 
 })();
 
